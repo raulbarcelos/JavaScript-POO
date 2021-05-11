acf98bd1673a17603652aed1746e6ed0ec6b527c
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-11 19:22:01 -0300

    Criando classe Cliente
    Cria classe cliente, gera uma instancia e atribui valores às propriedades da classe.

--- Mini Banco/index.js
+++ Mini Banco/index.js
@@ -1,2 +1,19 @@
-const cliente1 ="Raul"
+class Cliente{
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+cliente1.nome = "Cliente Exemplo";
+cliente1.cpf = 12312312375;
+cliente1.agencia = 3001;
+cliente1.saldo = 0;
+
+cliente1.nome = "Segundo Cliente Exemplo";
+cliente1.cpf = 32132132177;
+cliente1.agencia = 3001;
+cliente1.saldo = 122;
+
 console.log(cliente1)
