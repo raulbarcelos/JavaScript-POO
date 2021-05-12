b9660d9552ce2fd5740595422854bf467959815e
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-11 21:01:46 -0300

    Criando método sacar e depositar
    Adicionando funcionalidades para subtrair e somar valor ao saldo.

--- Mini Banco/index.js
+++ Mini Banco/index.js
@@ -1,21 +1,45 @@
 class Cliente{
     nome;
     cpf;
-    agencia;
-    saldo;
     rg;
     estado;
+}
+
+class ContaCorrente{
+    agencia;
+    conta;
+    saldo;
+
+    sacar(valor){
+        if(this.saldo >= valor){
+            this.saldo -= valor;
+        }else{
+            console.log("Não é possível sacar valor maior que o saldo.");
+        }
+    }
+    depositar(valor){
+        if(valor>0){
+            this.saldo += valor;
+        }else{
+            console.log("Não é possível depositar valor menor ou igual a 0.");
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 cliente1.nome = "Cliente Exemplo";
 cliente1.cpf = 12312312375;
-cliente1.agencia = 3001;
-cliente1.saldo = 0;
 
-cliente1.nome = "Segundo Cliente Exemplo";
-cliente1.cpf = 32132132177;
-cliente1.agencia = 3001;
-cliente1.saldo = 122;
+const contaCliente1 = new ContaCorrente();
+contaCliente1.agencia = 3001;
+contaCliente1.conta = 100098;
+contaCliente1.saldo = 150
 
-console.log(cliente1)
+console.log(contaCliente1.saldo)
+contaCliente1.sacar(200)
+console.log(contaCliente1.saldo)
+contaCliente1.sacar(80)
+console.log(contaCliente1.saldo)
+contaCliente1.depositar(950)
+console.log(contaCliente1.saldo)
+console.log(cliente1);
