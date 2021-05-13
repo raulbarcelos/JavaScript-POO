1b29d5c315373685aa68210bbbfa36e28ebeea66
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-13 17:59:03 -0300

    Encapsulando algumas variáveis na classe ContaCorrente
    Variável privada _cliente recebeu get e set e a _saldo recebeu o assessor get.

--- Mini Banco/ContaCorrente.js
+++ Mini Banco/ContaCorrente.js
@@ -1,9 +1,23 @@
+import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente{
     agencia;
-    conta;
-    cliente;
+    conta;    
+    _cliente;
     _saldo = 150;
+    
+    set cliente(novoValor){
+        if(novoValor instanceof Cliente)
+        this._cliente = novoValor;
+    }
+
+    get cliente(){
+        return this._cliente;
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
     
 
     sacar(valor){
