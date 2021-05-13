f3e07d466fab001eba0915b18f617f8db6477e7c
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-13 20:30:20 -0300

    Ajustando index.js
    Alterando os paramentos das instancias das classes ContaCorrente e ContaPoupanca

--- Mini Banco/index.js
+++ Mini Banco/index.js
@@ -5,9 +5,9 @@ import {ContaPoupanca} from "./ContaPoupanca.js"
 const cliente1 = new Cliente("Cliente exemplo 1", 12312312334);
 const cliente2 = new Cliente("Cliente Exemplo DOISS", 12313332215);
 
-const contaCliente1 = new ContaCorrente(3301, 123, cliente1);
-const contaCliente2 = new ContaCorrente(3301, 124, cliente2);
-const contaPoupanca1 = new ContaPoupanca(1500, cliente2, 2001)
+const contaCliente1 = new ContaCorrente(10000, cliente1, 3301);
+const contaCliente2 = new ContaCorrente(0, cliente2, 3301);
+const contaPoupanca1 = new ContaPoupanca(0, cliente2, 2001);
 
 contaCliente1.sacar(200);
 contaCliente1.sacar(80);
@@ -16,5 +16,5 @@ contaCliente1.transferir(500, contaCliente2);
 
 console.log(contaCliente1);
 console.log(contaCliente2);
+console.log(contaPoupanca1);
 console.log(ContaCorrente.numeroDeContas);
-console.log(contaPoupanca1)
