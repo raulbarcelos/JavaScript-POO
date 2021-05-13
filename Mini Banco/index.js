a3fee885f2539c97da37fa23da9b5da4613de4ba
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-13 00:06:02 -0300

    Importando Classes no index.js
    Criando arquivo package.json para setar o "type": "module"

--- Mini Banco/index.js
+++ Mini Banco/index.js
@@ -1,30 +1,5 @@
-class Cliente{
-    nome;
-    cpf;
-    rg;
-    estado;
-}
-
-class ContaCorrente{
-    agencia;
-    conta;
-    _saldo = 150;
-
-    sacar(valor){
-        if(this._saldo < valor){
-            return;
-        }
-
-        this._saldo -= valor;
-    }
-    depositar(valor){
-        if(valor<=0){
-            return;
-        }
-
-        this._saldo += valor;
-    }
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Cliente Exemplo";
@@ -37,4 +12,4 @@ contaCliente1.conta = 100098;
 contaCliente1.sacar(200)
 contaCliente1.sacar(80)
 contaCliente1.depositar(950)
-console.log(cliente1);
+console.log(cliente1, contaCliente1._saldo);
