927faf5787a12e48f613df87bfac11c2b1c9d48a
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-13 18:14:17 -0300

    Criando construtor na classe Cliente
    Para controle de estado criamos o construtor da classe cliente.

--- Mini Banco/Cliente.js
+++ Mini Banco/Cliente.js
@@ -1,6 +1,15 @@
 export class Cliente{
     nome;
-    cpf;
+    _cpf;
     rg;
     estado;
+
+    get cpf(){
+        return this._cpf
+    }
+
+    constructor(nome, cpf){
+        this.nome =  nome;
+        this._cpf = cpf;
+    }
 }
--- Mini Banco/index.js
+++ Mini Banco/index.js
@@ -1,28 +1,20 @@
 import {Cliente} from "./Cliente.js"
 import {ContaCorrente} from "./ContaCorrente.js"
 
-const cliente1 = new Cliente();
-cliente1.nome = "Cliente Exemplo";
-cliente1.cpf = 12312312375;
+const cliente1 = new Cliente("Cliente exemplo 1", 12312312334);
+const cliente2 = new Cliente("Cliente Exemplo DOISS", 12313332215);
 
 const contaCliente1 = new ContaCorrente();
 contaCliente1.agencia = 3001;
-contaCliente1.conta = 100098;
 contaCliente1.cliente = cliente1;
-
-contaCliente1.sacar(200)
-contaCliente1.sacar(80)
-contaCliente1.depositar(950)
-
-
-const cliente2 = new Cliente();
-cliente2.nome = "Cliente Exemplo DOISS";
-cliente2.cpf = 12313332215;
 
 const contaCliente2 = new ContaCorrente();
 contaCliente2.agencia = 3001;
-contaCliente2.conta = 198042;
 contaCliente2.cliente = cliente2;
+
+contaCliente1.sacar(200);
+contaCliente1.sacar(80);
+contaCliente1.depositar(950);
 contaCliente1.transferir(500, contaCliente2);
 
 console.log(contaCliente1);
