bce627ae59c52463bcf3cf1336b177d44a2494cd
raulbarcelos <raul.barcelos@ufvjm.edu.br>
2021-05-13 23:57:30 -0300

    Reescrita do index.js
    Apagamos o código anterior e criamos as novas instancias.

--- Mini Banco/index.js
+++ Mini Banco/index.js
@@ -1,20 +1,14 @@
 import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
-import {ContaPoupanca} from "./ContaPoupanca.js"
+import {Gerente} from "./Funcionarios/Gerente.js"
+import {Diretor} from "./Funcionarios/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Cliente exemplo 1", 12312312334);
-const cliente2 = new Cliente("Cliente Exemplo DOISS", 12313332215);
+const diretor1 = new Diretor("Raul", 9500, 12312312323);
+diretor1.cadastrarSenha("123123")
+const gerente1 = new Gerente("Diego", 5500, 32132131231);
 
-const contaCliente1 = new ContaCorrente(10000, cliente1, 3301);
-const contaCliente2 = new ContaCorrente(0, cliente2, 3301);
-const contaPoupanca1 = new ContaPoupanca(0, cliente2, 2001);
-
-contaCliente1.sacar(200);
-contaCliente1.sacar(80);
-contaCliente1.depositar(950);
-contaCliente1.transferir(500, contaCliente2);
-
-console.log(contaCliente1);
-console.log(contaCliente2);
-console.log(contaPoupanca1);
-console.log(ContaCorrente.numeroDeContas);
+const logado = SistemaAutenticacao.login(diretor1, "123123");
+const cliente = new Cliente("Josef", 123, "000123")
+const clilogado = SistemaAutenticacao.login(cliente, "000123");
+console.log(logado)
+console.log(clilogado)
